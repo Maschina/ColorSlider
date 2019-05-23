0652ecde154b6946e6c5654983adc68da159972d
Robert Hahn <robert.hahn@mailbox.org>
2019-05-23 22:11:49 +0200

    Initial Commit

---
+++ ColorSlider/ColorSlider.h
@@ -0,0 +1,19 @@
+//
+//  ColorSlider.h
+//  ColorSlider
+//
+//  Created by Robert Hahn on 23.05.19.
+//  Copyright © 2019 Robert Hahn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ColorSlider.
+FOUNDATION_EXPORT double ColorSliderVersionNumber;
+
+//! Project version string for ColorSlider.
+FOUNDATION_EXPORT const unsigned char ColorSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorSlider/PublicHeader.h>
+
+
